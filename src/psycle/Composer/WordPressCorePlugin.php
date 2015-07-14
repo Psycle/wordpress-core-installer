34a1464f51a1f671d78b076d473bde3d8487956c
James Robinson <jscrobinson@gmail.com>
2015-07-14 13:16:44 +0100

    Updated composer

--- src/psycle/Composer/WordPressCorePlugin.php
+++ src/psycle/Composer/WordPressCorePlugin.php
@@ -5,8 +5,6 @@ namespace psycle\Composer;
 use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
-use Composer\Script\ScriptEvents;
-use \Composer\Installer\PackageEvent;
 
 class WordPressCorePlugin implements PluginInterface {
 
@@ -19,23 +17,5 @@ class WordPressCorePlugin implements PluginInterface {
 	public function activate( Composer $composer, IOInterface $io ) {
 		$installer = new WordPressCoreInstaller( $io, $composer );
 		$composer->getInstallationManager()->addInstaller( $installer );		
-	}
-	
-	public static function getSubscribedEvents()
-    {
-        return array(
-            ScriptEvents::POST_PACKAGE_INSTALL => array(
-                array('onPreFileDownload', 0)
-            ),
-			ScriptEvents::POST_PACKAGE_UPDATE => array(
-				array('onPostPackageUpdate', 0)
-			),
-        );
-    }
-	
-	public function onPostPackageUpdate(PackageEvent $event) {
-		var_dump($event->getName());
-		
-		die(var_dump($event));
 	}
 }
